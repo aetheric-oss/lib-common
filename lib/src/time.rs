b515c975a3e4d77cfa15980e715249f902849892
A.M. Smith <smitham42@gmail.com>
2024-05-30 07:39:53 -0700

    feat: add LocalResult and SecondsFormat

--- lib/src/time.rs
+++ lib/src/time.rs
@@ -2,6 +2,7 @@
 
 pub use chrono::{
     naive::{NaiveDate, NaiveDateTime},
-    DateTime, Datelike, Duration, Local, TimeZone, Timelike, Utc,
+    DateTime, Datelike, Duration, Local, LocalResult, SecondsFormat, TimeZone, Timelike, Utc,
 };
+pub use chrono_tz;
 pub use prost_wkt_types::Timestamp;
